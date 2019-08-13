9c0b613ce1640b8dbce44d5a0462aaf728301b66
Brian <riceb53@gmail.com>
2019-08-12 20:16:03 -0500

    solve fizzbuzz TDD style

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,6 +1,13 @@
 class FizzBuzz
   def output(number)
-    return 1
+    if number % 3 == 0 && number % 5 == 0
+      return 'fizz_buzz'
+    elsif number % 5 == 0
+      return 'buzz'
+    elsif number % 3 == 0
+      return 'fizz'
+    end
+    return number
   end
 end
 
@@ -10,6 +17,41 @@ RSpec.describe FizzBuzz do
       fizz_buzz = FizzBuzz.new
       result = fizz_buzz.output(1)
       expect(result).to eq(1)
+    end
+    it 'should return 2 when given 2' do
+      fizz_buzz = FizzBuzz.new
+      result = fizz_buzz.output(2)
+      expect(result).to eq(2)
+    end
+    it 'should return \'fizz\' when given 3' do
+      fizz_buzz = FizzBuzz.new
+      result = fizz_buzz.output(3)
+      expect(result).to eq('fizz')
+    end
+    it 'should return 4 when given 4' do
+      fizz_buzz = FizzBuzz.new
+      result = fizz_buzz.output(4)
+      expect(result).to eq(4)
+    end
+    it 'should return \'buzz\' when given 5' do
+      fizz_buzz = FizzBuzz.new
+      result = fizz_buzz.output(5)
+      expect(result).to eq('buzz')
+    end
+    it 'should return \'fizz\' when given 6' do
+      fizz_buzz = FizzBuzz.new
+      result = fizz_buzz.output(6)
+      expect(result).to eq('fizz')
+    end
+    it 'should return \'buzz\' when given 10' do
+      fizz_buzz = FizzBuzz.new
+      result = fizz_buzz.output(10)
+      expect(result).to eq('buzz')
+    end
+    it 'should return \'fizz_buzz\' when given 15' do
+      fizz_buzz = FizzBuzz.new
+      result = fizz_buzz.output(15)
+      expect(result).to eq('fizz_buzz')
     end
   end
 end
